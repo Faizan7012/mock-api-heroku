822e587c98e474b5a0af23a1de3f06de377561cf
Faizan7012 <frtoons7012@gmail.com>
2022-11-08 15:33:24 +0530

    added this file

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const data = require('db.json')
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 3001
+
+server.use(middleware);
+server.use(router);
+
+
+server.listen(port)
